ab6961bdeb5652314f0f1b78a6a528f868233df8
evaldo <evaaldormj@gmail.com>
2025-02-27 19:37:48 -0300

    Feat: Add calculate function to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,44 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
+import { UserInputComponent } from "./user-input/user-input.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, HeaderComponent],
+  imports: [CommonModule, HeaderComponent, UserInputComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'INVESTMENTS-CALCULATOR';
+
+  onCalculateInvestmentResults(data: {
+    initialInvestment: number,
+    annualInvestment: number,
+    expectedReturn: number,
+    duration: number
+  }) {
+      const { initialInvestment, annualInvestment, expectedReturn, duration } = data;
+      const annualData = [];
+      let investmentValue = initialInvestment;
+
+      for (let i = 0; i < duration; i++) {
+        const year = i + 1;
+        const interestEarnedInYear = investmentValue * (expectedReturn / 100);
+        investmentValue += interestEarnedInYear + annualInvestment;
+        const totalInterest =
+          investmentValue - annualInvestment * year - initialInvestment;
+        annualData.push({
+          year: year,
+          interest: interestEarnedInYear,
+          valueEndOfYear: investmentValue,
+          annualInvestment: annualInvestment,
+          totalInterest: totalInterest,
+          totalAmountInvested: initialInvestment + annualInvestment * year,
+        });
+      }
+
+      console.log(annualData);
+    }
 }
